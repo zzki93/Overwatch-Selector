5d191ece31f8227eec7ef328fc1cface74ca9161
zzki93 <rlaqudrms12@gmail.com>
2022-05-02 12:22:32 +0900

    오버워치 캐릭터 선택창

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const linked = document.querySelector("a")
+
+function onLoginSubmit(event){
+  event.preventDefault();
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
+
+function handleClicked(event){
+  event.preventDefault();
+}
+
+linked.addEventListener("click",handleClicked);
+
+
+
+
